a302392fa4f9db8a3a212e06f38a8749a519090d
rodicornelisse <45554179+rodicornelisse@users.noreply.github.com>
2019-02-17 18:12:44 +0100

    Add files via upload

---
+++ v1/js/script.js
@@ -0,0 +1,45 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+
+//function dragover_handler(ev) {
+//ev.preventDefault();
+// Set the dropEffect to move
+//ev.dataTransfer.dropEffect = "move";
+//}
+
+
+//function drop_handler(ev) {
+//ev.preventDefault();
+// Get the id of the target and add the moved element to the target's DOM
+//var data = ev.dataTransfer.getData("text/plain");
+// ev.target.appendChild(document.getElementById(data));
+//}
+
+//dragover_handler();
+//drop_handler();
+
+
+function dragstart_handler(ev) {
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    ev.dropEffect = "move";
+}
+
+function dragover_handler(ev) {
+    ev.preventDefault();
+    // Set the dropEffect to move
+    ev.dataTransfer.dropEffect = "move";
+}
+
+function drop_handler(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    var data = ev.dataTransfer.getData("text/plain");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+dragstart_handler();
+dragover_handler();
+drop_handler();
